937ca6f6463dca45a0b872e97c10a9fbc5966df3
Dan Munro <dan@danmunro.com>
2019-12-25 19:49:09 -0600

    delete unused file

--- app.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import proxy from 'http-proxy-middleware';
-
-const app = express();
-
-// app.use('/user/:uuid/suggested-follows', proxy({ target: 'localhost:8081' }));
-app.use('/session', proxy({ target: 'http://localhost:8080' }));
-app.listen(3000);
