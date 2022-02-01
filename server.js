228194d47aa513098a856794a3186f563fbf704e
Dan Munro <dan@danmunro.com>
2022-01-31 20:00:00 -0800

    add eps

--- server.js
+++ server.js
@@ -29,6 +29,8 @@ app.use('/user/:username/follows', proxy({ target: targets.communityService }));
 app.use('/user/:username/followers', proxy({ target: targets.communityService }));
 app.use('/user/:username/suggested-follows', proxy({ target: targets.communityService }));
 app.use('/post', proxy({ target: targets.communityService }));
+app.use('/post/:uuid', proxy({ target: targets.communityService }));
+app.use('/post/:uuid/like', proxy({ target: targets.communityService }));
 app.use('/reply', proxy({ target: targets.communityService }));
 app.use('/follow/:uuid', proxy({ target: targets.communityService }));
 
